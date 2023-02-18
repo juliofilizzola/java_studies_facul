bac984b5057fdabf93242815304069c442b63d55
julio_filizzola <juliofilizzola@hotmail.com>
2023-02-18 12:05:18 -0300

    Feat: create var name

--- String/String/src/Main.java
+++ String/String/src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        String name = "julio";
         StringTest stringTest = new StringTest();
-        System.out.println(stringTest.equalsTest("julio"));
-        System.out.println(stringTest.equalsIgnoreCaseTest("julio"));
+        System.out.println(stringTest.equalsTest(name));
+        System.out.println(stringTest.equalsIgnoreCaseTest(name));
     }
 }
