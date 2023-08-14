832b565bb072a9076f1c8ecbedd0e18f1c5c0b54
Julio Filizzola <juliofilizzola@Hotmail.com>
2023-08-13 21:20:19 -0300

    feat: create example

---
+++ matrix-vetor/src/Matriz/Matriz.java
@@ -0,0 +1,21 @@
+package Matriz;
+
+import javax.swing.*;
+
+public class 1 {
+public void MatrixEx() {
+	System.out.println("___Matrix___");
+	
+	int MatSoma[] [], a, b, soma = 0;
+	
+	MatSoma = new int[10][12];
+	
+	for (a = 0; a <= 9; a++) {
+		for (b = 0; b <= 11; b++) {
+			MatSoma[a] [b] = Integer.parseInt(JOptionPane.showInputDialog("Digite um valor inteiro"));
+			soma = soma + MatSoma [a] [b];
+		}
+	}
+	System.out.println("O resultado é: " + soma);
+}
+}
